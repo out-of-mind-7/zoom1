21dec85cc83551f7f091a03dea18fac535bad340
Abhijith <abhijithsantosh234@gmail.com>
2025-02-26 02:36:59 +0530

    Update WebSocket path and fix imports

--- zoom1/settings.py
+++ zoom1/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+# Print the PORT environment variable
+print("PORT:", os.getenv('PORT'))
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
